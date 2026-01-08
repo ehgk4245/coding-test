48d8c72eada4d4120a1bdfa6c8674e14fba43d00
김도하 <dohakim.dev@gmail.com>
2026-01-08 20:10:34 +0900

    [level 2] Title: 숫자의 표현, Time: 1.34 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,19 +1,18 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
-        outer:
         for (int i = 1; i <= n; i++) {
-            int sum = 0;
-            for (int j = i; j <= n; j++) {
-                sum += j;
+            int sum = i;
+            int count = 1;
+            while (sum <= n) {
                 if (sum == n) {
                     answer++;
-                    continue outer;
-                } else if (sum > n) continue outer;
+                    break;
+                }
+                
+                sum += i + count++;
             }
         }
-        
         return answer;
     }
 }
