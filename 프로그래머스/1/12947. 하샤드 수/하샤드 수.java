3a402a5260161aecfe37246d1af397e584a33e38
김도하 <dohakim.dev@gmail.com>
2026-01-10 13:46:33 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int tmp = x;
+        int sum = 0;
+        while (tmp > 0) {
+            sum += tmp % 10;
+            tmp /= 10;
+        }
+        
+        if (x % sum == 0) return true;
+        return false;
+    }
+}
